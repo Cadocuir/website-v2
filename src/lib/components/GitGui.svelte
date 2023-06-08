3c5f564d7a095901ba8071793e84d2cf30c02f64
GuillianV <guillianvibert90@gmail.com>
2023-06-08 10:03:42 +0200

    feat: init project

---
+++ src/lib/components/GitGui.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { onMount } from 'svelte';
+    import {GitGuiScene} from "$lib/three/gitgui.scene.js";
+
+    
+	let el;
+
+    onMount(() => {
+        init()
+    });
+
+    async function init() {
+        let gltfScene = new GitGuiScene({
+            width: window.innerWidth,
+            height: window.innerHeight
+        })
+        gltfScene.createScene(el)
+    }
+
+</script>
+
+
+<canvas bind:this={el} />
+
+
+<style lang="scss">
+
+canvas{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top:0;
+    left:0;
+    z-index: -5;
+
+}
+
+
+</style>
---
+++ src/routes/(portfolio)/+page.svelte
@@ -0,0 +1,47 @@
+
+<script>
+
+    import { setting_ldm_store } from '$lib/settings/detailMode.js'
+    import H1 from "$lib/components/H1.svelte";
+  //  import GitGui from "$lib/components/GitGui.svelte";
+    import { t } from '$lib/translations';
+
+
+</script>
+
+<div id="top">
+
+
+    <H1 innerText="{$t('home.titre')}" />
+
+    <p>{$t('home.sous-titre')}</p>
+
+</div>
+
+<a href="/about">a propos</a>
+{#if $setting_ldm_store > 0}
+    <!-- //<GitGui /> -->
+{/if}
+
+<style lang="scss">
+
+#top{
+
+    	
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    
+    p{
+        font-size: 1.5rem;
+        margin: 0;
+        font-family: $font-family-secondary;
+        color: $color-light;
+
+    }
+}
+
+</style>
