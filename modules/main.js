1066840c3634d358053b1a2f6cc947d302f4d1c3
GuillianV <guillianvibert90@gmail.com>
2024-09-08 01:14:41 +0200

    fix

--- modules/main.js
+++ modules/main.js
@@ -19,7 +19,7 @@ export const fetchInstagramLatestData = async () => {
         const browser = await puppeteer.launch({
             headless: true,
             executablePath: process.env.CHROME_BIN || undefined,
-            args: ['--no-sandbox','--disable-gpu','--disable-dev-shm-usage','--headless']
+            args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--headless']
         });
         const page = await browser.newPage();
 
@@ -55,5 +55,63 @@ export const fetchInstagramLatestData = async () => {
 }
 
 (async () => {
-    await fetchInstagramLatestData()
+    // await fetchInstagramLatestData()
+})();
+
+
+(async () => {
+
+
+    try {
+        if (INSTAGRAM_USER == null || INSTAGRAM_PASSWORD == null) {
+            throw Error("Missing env Vars INSTAGRAM_USER INSTAGRAM_PASSWORD")
+        }
+
+        const browser = await puppeteer.launch({
+            headless: false,
+            executablePath: process.env.CHROME_BIN || undefined,
+            args: ['--no-sandbox', '--disable-gpu']
+        });
+        const page = await browser.newPage();
+
+        // Wait until page has loaded
+
+        await page.goto('https://www.instagram.com/accounts/login/', {
+            waitUntil: 'networkidle0',
+        });
+
+        // Wait for log in form
+
+        await Promise.all([
+            page.waitForSelector('input[name="username"]'),
+            page.waitForSelector('input[name="password"]'),
+            page.waitForSelector('button[type="submit"]'),
+        ]);
+
+        // Enter username and password
+        await page.click("._a9--._ap36._a9_0")
+        await page.type('input[name="username"]', INSTAGRAM_USER);
+        await page.type('input[name="password"]', INSTAGRAM_PASSWORD);
+        await new Promise((res,err)=>{setTimeout(_=>{res()},1000)})
+        await page.waitForSelector('button[type="submit"]:not([disabled])',{visible:true})
+        
+        // Submit log in credentials and wait for navigation
+        await page.click('button[type="submit"]')
+        await page.waitForNavigation({
+            waitUntil: 'networkidle0',
+        }),
+
+
+            // Download PDF
+
+            await page.pdf({
+                path: 'uploads/page.pdf',
+                format: 'A4',
+            });
+
+        await browser.close();
+
+    } catch (e) {
+        console.log(e)
+    }
 })();
